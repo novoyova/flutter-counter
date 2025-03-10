736ba93331434f4d33155a2c6c2fb6363246b046
kevin.horasia <kevin.horasia@kkday.com>
2025-03-10 22:53:33 +0800

    [UPD] upgrade flutter project

--- lib/presentation/screens/counter_screen/widgets/animated_circle.dart
+++ lib/presentation/screens/counter_screen/widgets/animated_circle.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
+// import 'package:flare_flutter/flare_actor.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class AnimatedCircle extends StatelessWidget {
@@ -12,14 +12,19 @@ class AnimatedCircle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: 70.0.w,
       height: 40.0.h,
-      child: FlareActor(
-        'assets/flares/circle.flr',
-        animation: animate ? 'Alarm' : null,
-        color: Theme.of(context).colorScheme.secondary,
-      ),
+      color: Theme.of(context).colorScheme.secondary,
     );
+    // return SizedBox(
+    //   width: 70.0.w,
+    //   height: 40.0.h,
+    //   child: FlareActor(
+    //     'assets/flares/circle.flr',
+    //     animation: animate ? 'Alarm' : null,
+    //     color: Theme.of(context).colorScheme.secondary,
+    //   ),
+    // );
   }
 }
